7a1debba7c86e86cc00416a9fb8107a405ad9a5e
William Hsieh <william.hsieh@kronosresearch.com>
2025-07-29 11:52:36 +0000

    refactor(api): rename promote functions to `open_in_*`

--- lua/overlook/api.lua
+++ lua/overlook/api.lua
@@ -1,5 +1,6 @@
 local Peek = require("overlook.peek")
 local Stack = require("overlook.stack")
+local Ui = require("overlook.ui")
 
 local M = {}
 
@@ -43,60 +44,30 @@ M.close_all = function()
 end
 
 --- Promotes the top Overlook popup to a regular window (split, vsplit, or tab).
---- @param open_command string Command to run when promoting the popup to a window.
-local function promote_popup_to_window(open_command)
-  if Stack.empty() or not vim.w.is_overlook_popup then
-    vim.notify("Overlook: No popup to promote.", vim.log.levels.INFO)
-    return
-  end
-
-  local buf_id_to_open = vim.api.nvim_get_current_buf()
-
-  ---@diagnostic disable-next-line: unused-local
-  local _bufnum, lnum, col, _off = unpack(vim.fn.getpos("."))
-
-  -- Close all overlook popups. This also clears the stack.
-  Stack.clear()
-
-  -- Ensure the buffer is still valid and positive before trying to open it
-  if not buf_id_to_open or not vim.api.nvim_buf_is_valid(buf_id_to_open) then
-    vim.notify(
-      string.format("Overlook Error: Buffer to promote is invalid (ID: %s).", tostring(buf_id_to_open)),
-      vim.log.levels.ERROR
-    )
-    return
-  end
-
-  -- Open the buffer in the specified way
-  local cmd = string.format("%s %d", open_command, buf_id_to_open)
-  ---@diagnostic disable-next-line: param-type-mismatch
-  local ok, err = pcall(vim.cmd, cmd)
-  if not ok then
-    vim.notify(
-      string.format("Overlook Error: Failed to execute command '%s': %s", cmd, tostring(err)),
-      vim.log.levels.ERROR
-    )
-    return -- Ensure we don't proceed if window creation failed
-  end
-
-  -- set cursor position in the new window
-  -- TODO: extract this to a util function
-  vim.api.nvim_win_set_cursor(0, { lnum, math.max(0, col - 1) })
-  vim.api.nvim_win_call(0, function()
-    vim.cmd("normal! zz")
-  end)
+--- @param open_command 'vsplit'|'split'|'tabnew' Vim command to open the window.
+local promote_top_to_window = function(open_command)
+  local cmd = string.format("%s | buffer", open_command)
+  Ui.promote_popup_to_window(cmd)
 end
 
---- Promotes the top Overlook popup to a regular window (split, vsplit, or tab).
---- @param open_command 'vsplit'|'split'|'tabnew' Vim command to open the window.
-M.promote_top_to_window = function(open_command)
-  local cmd = string.format("%s | buffer", open_command)
-  promote_popup_to_window(cmd)
+--- Opens the top Overlook popup in a new split window.
+M.open_in_split = function()
+  promote_top_to_window("split")
+end
+
+--- Opens the top Overlook popup in a new vertical split window.
+M.open_in_vsplit = function()
+  promote_top_to_window("vsplit")
+end
+
+--- Opens the top Overlook popup in a new tab.
+M.open_in_tab = function()
+  promote_top_to_window("tabnew")
 end
 
 --- Promotes the top Overlook popup to the root window.
-M.promote_top_to_original_window = function()
-  promote_popup_to_window("buffer")
+M.open_in_original_window = function()
+  Ui.promote_popup_to_window("buffer")
 end
 
 return M
--- lua/overlook/ui.lua
+++ lua/overlook/ui.lua
@@ -15,4 +15,51 @@ function M.create_popup(opts)
   return popup
 end
 
+--- Promotes the top Overlook popup to a regular window (split, vsplit, or tab).
+--- @param open_command string Command to run when promoting the popup to a window.
+function M.promote_popup_to_window(open_command)
+  local stack = require("overlook.stack").get_current_stack()
+
+  if not stack or stack:empty() or not vim.w.is_overlook_popup then
+    vim.notify("Overlook: No popup to promote.", vim.log.levels.INFO)
+    return
+  end
+
+  local buf_id_to_open = vim.api.nvim_get_current_buf()
+
+  ---@diagnostic disable-next-line: unused-local
+  local _bufnum, lnum, col, _off = unpack(vim.fn.getpos("."))
+
+  -- Close all overlook popups. This also clears the stack.
+  stack:clear()
+
+  -- Ensure the buffer is still valid and positive before trying to open it
+  if not buf_id_to_open or not vim.api.nvim_buf_is_valid(buf_id_to_open) then
+    vim.notify(
+      string.format("Overlook Error: Buffer to promote is invalid (ID: %s).", tostring(buf_id_to_open)),
+      vim.log.levels.ERROR
+    )
+    return
+  end
+
+  -- Open the buffer in the specified way
+  local cmd = string.format("%s %d", open_command, buf_id_to_open)
+  ---@diagnostic disable-next-line: param-type-mismatch
+  local ok, err = pcall(vim.cmd, cmd)
+  if not ok then
+    vim.notify(
+      string.format("Overlook Error: Failed to execute command '%s': %s", cmd, tostring(err)),
+      vim.log.levels.ERROR
+    )
+    return -- Ensure we don't proceed if window creation failed
+  end
+
+  -- set cursor position in the new window
+  -- TODO: extract this to a util function
+  vim.api.nvim_win_set_cursor(0, { lnum, math.max(0, col - 1) })
+  vim.api.nvim_win_call(0, function()
+    vim.cmd("normal! zz")
+  end)
+end
+
 return M
